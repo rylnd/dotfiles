e1d06b5f49593c89a538bd9c06ab9d3d5ba61545
Ryland Herrick <ryalnd@gmail.com>
2017-05-12 15:44:31 -0500

    Update a missed test with newer shpec syntax

--- shpec/setup/pair_shpec.sh
+++ shpec/setup/pair_shpec.sh
@@ -1,6 +1,7 @@
 source $SHPEC_ROOT/../setup/pair
 
-## SETUP
+describe "setup_pair"
+  ## SETUP
   backup_files() {
     [[ -e $HOME/.bash_profile ]] && back_bash=true
     [[ -e $HOME/.pair ]] && back_pair=true
@@ -10,23 +11,24 @@ source $SHPEC_ROOT/../setup/pair
     touch $HOME/.bash_profile
   }
   backup_files
-
   pair_hook="$(pair_hook)"
 
-describe "setup_pair"
   it "adds to .bash_profile"
     modify_dotfile &> /dev/null
     added_lines="$(cat $HOME/.bash_profile)"
     assert equal "$added_lines" "$pair_hook"
+  end
 
   it "is idempotent"
     modify_dotfile &> /dev/null
     added_lines="$(cat $HOME/.bash_profile)"
     assert equal "$added_lines" "$pair_hook"
+  end
 
   it "symlinks from the repo"
     symlink_pair
     assert test "[[ -L $HOME/.pair ]]"
+  end
 
   it "symlinks to an absolute path"
     symlink_pair &> /dev/null
@@ -37,8 +39,9 @@ describe "setup_pair"
     popd &> /dev/null
 
     assert symlink "$HOME/.pair" "$expected_path"
-end_describe
+  end
 
-## TEARDOWN
+  ## TEARDOWN
   [[ -n "$back_bash" ]] && mv $HOME/.bash_profile{.bak,} || rm $HOME/.bash_profile
   [[ -n "$back_pair" ]] && mv $HOME/.pair{.bak,} || rm $HOME/.pair
+end
